1ec3f57acefcbccf42c05a7108fc936be4582b8b
Ayoub <ayoubsolicode07@gmail.com>
2024-11-29 15:58:02 +0000

    organize folder

--- Unit-3 Display lists and use Material Design/app/src/main/java/Tuto/tuo.kt
+++
@@ -1,6 +0,0 @@
-
-fun main () {
-    val ayoub = "Hello world"
-    println(ayoub)
-
-}
--- Unit-3 Display lists and use Material Design/app/src/main/java/Tuto/tuto2.kt
+++
@@ -1,6 +0,0 @@
-fun main () {
-    var count = 2
-    count ++
-    println("you have $count unread message!!")
-
-}
