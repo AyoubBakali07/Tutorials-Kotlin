1c4b9c9fe0c160a09ec74a2da91fe3e07da5553f
Ayoub <ayoubsolicode07@gmail.com>
2024-12-05 16:21:31 +0000

    live coding

---
+++ Unit-1 Your first Android app/Live_Coding/app/src/main/java/com/example/live_coding/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.live_coding
+
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.live_coding.ui.theme.Live_codingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Live_codingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CarteDeVisite()
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun CarteDeVisite() {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "John DOE",
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = "Developer Web",
+            style = MaterialTheme.typography.titleMedium,
+            color = Color.Gray
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Contact Info
+        ContactInfo(
+            label = "Telephone",
+            value = "+2126751752",
+            onClick = {
+                val callIntent = Intent(Intent.ACTION_DIAL).apply {
+                    data = Uri.parse("tel:+2126751752")
+                }
+                context.startActivity(callIntent)
+            }
+        )
+        ContactInfo(
+            label = "Email",
+            value = "text@mail.com",
+            onClick = {
+                val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                    data = Uri.parse("mailto:text@mail.com")
+                }
+                context.startActivity(emailIntent)
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Button for Website
+        Button(onClick = {
+            val websiteIntent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://www.johndoe.com")
+            }
+            context.startActivity(websiteIntent)
+        }) {
+            Text("Visit my Website")
+        }
+    }
+}
+
+@Composable
+fun ContactInfo(label: String, value: String, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .clickable(onClick = onClick)
+            .padding(vertical = 15.dp)
+    ) {
+        Text(text = "$label: ", fontWeight = FontWeight.Bold)
+        Text(text = value, color = Color.blue)
+    }
+}
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    Live_codingTheme {
+        CarteDeVisite()
+    }
+}
